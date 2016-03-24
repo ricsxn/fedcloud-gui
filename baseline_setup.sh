499cd93db27d514c905bf5d1afefbefea6701aa6
ricsxn <riccardo.bruno@gmail.com>
2016-03-24 15:03:17 +0000

    change in baseline.sh

--- baseline_setup.sh
+++ baseline_setup.sh
@@ -11,7 +11,7 @@ EOF
 
 cat > occi_endpoints <<EOF
 stack-01 https://stack-server-01.ct.infn.it:8787
-stack-02 https://stack-server-02.ct.infn.it:8787
+stack-02 http://stack-server-02.ct.infn.it:8787
 cesnet https://carach5.ics.muni.cz:11443
 nebula-01 https://nebula-server-01.ct.infn.it:9000
 ceta-grid https://controller.ceta-ciemat.es:8787
